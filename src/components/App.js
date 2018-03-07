9886921d99429b8e47ef8b32aec90a35334e541f
Manjunatha D <manjunathad13@gmail.com>
2018-03-07 18:49:30 +0530

    Load more products on click of a button

--- src/components/App.js
+++ src/components/App.js
@@ -2,92 +2,114 @@ import React from 'react';
 import Product from './Product.js';
 
 const DATA_URL =
-  'https://www.wooplr.com/rest/v2/advancedlookup/productsnew/top?pageNumber=1&count=20&fromCache=true';
+	'https://www.wooplr.com/rest/v2/advancedlookup/productsnew/top?count=20&fromCache=true';
 const BASE_IMAGE_URL =
-  'https://res.wooplr.com/image/upload/f_auto,q_auto,fl_lossy,w_200,c_fill,ar_3:4/';
+	'https://res.wooplr.com/image/upload/f_auto,q_auto,fl_lossy,w_200,c_fill,ar_3:4/';
 const BASE_WEBLINK_URL = 'https://www.wooplr.com';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      products: []
-    };
-  }
+		this.state = {
+			products: [],
+			count: 1
+		};
+	}
 
-  render() {
-    return (
-      <div>{this.state.products.map((product) => <Product key={product.id} {...product} />)}</div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				{this.state.products.map(product => (
+					<Product key={product.id} {...product} />
+				))}
+				<button
+					onClick={() => {
+						let count = this.state.count + 1;
+						this.setState(() => ({ count }));
+						this.fetchData(count).then(newProducts => {
+							let products = [...this.state.products, ...newProducts];
+							this.setState(() => ({ products }));
+						});
+					}}
+				>
+					Load More
+				</button>
+			</div>
+		);
+	}
 
-  componentDidMount() {
-    const headers = {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    };
+	componentDidMount() {
+		this.fetchData(this.state.count).then(products =>
+			this.setState(() => ({ products }))
+		);
+	}
 
-    const body = JSON.stringify({
-      newFilters: [],
-      sort_by: ['relevance']
-    });
+	fetchData(page) {
+		const fetchUrl = `${DATA_URL}&pageNumber=${page}`;
 
-    fetch(DATA_URL, {
-      method: 'POST',
-      headers,
-      body
-    })
-      .then((response) => response.json())
-      .then((response) => {
-        let products = response
-          .filter((item) => item.id !== -2) // remove the search metadata object from the list of products
-          .map((item) => item.ecommerceProductJAXB)
-          .map(
-            // get only the properties you need
-            ({
-              id,
-              name,
-              description,
-              image_pid,
-              salesPrice,
-              retailPrice,
-              discount,
-              color,
-              size,
-              availableSizes,
-              stock,
-              available,
-              category,
-              subCategory,
-              gender,
-              webLink
-            }) => ({
-              id,
-              name,
-              description,
-              image_pid: image_pid.map((imageUrl) => BASE_IMAGE_URL.concat(imageUrl)),
-              salesPrice,
-              retailPrice,
-              discount,
-              color,
-              size,
-              availableSizes,
-              stock,
-              available,
-              category,
-              subCategory,
-              gender,
-              webLink: BASE_WEBLINK_URL.concat(webLink)
-            })
-          );
+		const headers = {
+			Accept: 'application/json',
+			'Content-Type': 'application/json'
+		};
 
-        console.log('Response', response);
-        console.log('products', products);
+		const body = JSON.stringify({
+			newFilters: [],
+			sort_by: ['relevance']
+		});
 
-        this.setState(() => ({ products }));
-      });
-  }
+		return fetch(fetchUrl, {
+			method: 'POST',
+			headers,
+			body
+		})
+			.then(response => response.json())
+			.then(response =>
+				response
+					.filter(item => item.id !== -2) // remove the search metadata object from the list of products
+					.map(item => item.ecommerceProductJAXB)
+					.map(
+						// get only the properties you need
+						({
+							id,
+							name,
+							description,
+							image_pid,
+							salesPrice,
+							retailPrice,
+							discount,
+							color,
+							size,
+							availableSizes,
+							stock,
+							available,
+							category,
+							subCategory,
+							gender,
+							webLink
+						}) => ({
+							id,
+							name,
+							description,
+							image_pid: image_pid.map(imageUrl =>
+								BASE_IMAGE_URL.concat(imageUrl)
+							),
+							salesPrice,
+							retailPrice,
+							discount,
+							color,
+							size,
+							availableSizes,
+							stock,
+							available,
+							category,
+							subCategory,
+							gender,
+							webLink: BASE_WEBLINK_URL.concat(webLink)
+						})
+					)
+			);
+	}
 }
 
 export default App;
