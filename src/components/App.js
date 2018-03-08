5177f6b253ec04c8aa4bd4a997e7d13c2ab7654d
Manjunatha D <manjunathad13@gmail.com>
2018-03-09 00:07:48 +0530

    Filters WIP

--- src/components/App.js
+++ src/components/App.js
@@ -3,87 +3,138 @@ import OverlayLoader from 'react-overlay-loading/lib/OverlayLoader';
 import Product from './Product.js';
 
 const DATA_URL =
-  'https://www.wooplr.com/rest/v2/advancedlookup/productsnew/top?count=20&fromCache=true';
+	'https://www.wooplr.com/rest/v2/advancedlookup/productsnew/top?count=20&fromCache=true';
 const BASE_IMAGE_URL =
-  'https://res.wooplr.com/image/upload/f_auto,q_auto,fl_lossy,w_200,c_fill,ar_3:4/';
+	'https://res.wooplr.com/image/upload/f_auto,q_auto,fl_lossy,w_200,c_fill,ar_3:4/';
 const BASE_WEBLINK_URL = 'https://www.wooplr.com';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      products: [],
-      count: 1,
-      loading: true,
-      columns: 3,
-      filters: {
-        gender: '',
-        size: '',
-        color: ''
-      }
-    };
+		this.state = {
+			products: [],
+			filteredProducts: [],
+			count: 1,
+			loading: true,
+			columns: 3,
+			filters: {
+				gender: '',
+				size: '',
+				color: ''
+			}
+		};
 
-    this.fetchData = this.fetchData.bind(this);
-    this.updateProducts = this.updateProducts.bind(this);
-    this.handleScroll = this.handleScroll.bind(this);
-  }
+		this.fetchData = this.fetchData.bind(this);
+		this.updateProducts = this.updateProducts.bind(this);
+		this.handleScroll = this.handleScroll.bind(this);
+	}
 
-  updateColor = (event) =>
-    this.setState({ filters: { ...this.state.filters, color: event.target.value } });
+	updateColor = event =>
+		this.setState({
+			filters: { ...this.state.filters, color: event.target.value }
+		});
 
-  render() {
-    return (
-      <OverlayLoader
-        color={'black'}
-        loader="ScaleLoader"
-        text="Loading more products... Please wait!"
-        active={this.state.loading}
-        backgroundColor={'black'}
-        opacity=".3"
-      >
-        <div className="page-title">Buy on my Wooplr Store!</div>
-        <div className="filter-bar">
-          <div id="sizeFilter" className="button-group">
-            <button
-              className={this.state.filters.size === 'S' ? 'active' : ''}
-              onClick={() =>
-                this.setState(() => ({ filters: { ...this.state.filters, size: 'S' } }))
-              }
-            >
-              S
-            </button>
-            <button
-              className={this.state.filters.size === 'M' ? 'active' : ''}
-              onClick={() =>
-                this.setState(() => ({ filters: { ...this.state.filters, size: 'M' } }))
-              }
-            >
-              M
-            </button>
-            <button
-              className={this.state.filters.size === 'L' ? 'active' : ''}
-              onClick={() => this.setState({ filters: { ...this.state.filters, size: 'L' } })}
-            >
-              L
-            </button>
-            <button
-              className={this.state.filters.size === 'XL' ? 'active' : ''}
-              onClick={() =>
-                this.setState(() => ({ filters: { ...this.state.filters, size: 'XL' } }))
-              }
-            >
-              XL
-            </button>
-            <button
-              className={this.state.filters.size === 'XXL' ? 'active' : ''}
-              onClick={() =>
-                this.setState(() => ({ filters: { ...this.state.filters, size: 'XXL' } }))
-              }
-            >
-              XXL
-            </button>
-          </div>
+	updateGender = gender => {
+		console.log('update gender called');
+		let filteredProducts = this.filterByGender(gender);
+		this.setState({
+			filters: { ...this.state.filters, gender },
+			filteredProducts
+		});
+	};
+
+	render() {
+		return (
+			<OverlayLoader
+				color={'black'}
+				loader="ScaleLoader"
+				text="Loading more products... Please wait!"
+				active={this.state.loading}
+				backgroundColor={'black'}
+				opacity=".3"
+			>
+				<div className="page-title">Buy on my Wooplr Store!</div>
+				<div className="filter-bar">
+					<div id="sizeFilter" className="button-group">
+						<button
+							className={this.state.filters.size === 'S' ? 'active' : ''}
+							onClick={() => {
+								console.log('s clicked');
+								let filteredProducts = this.filterBySize(
+									this.state.filteredProducts,
+									'S'
+								);
+								this.setState(() => ({
+									filters: { ...this.state.filters, size: 'S' },
+									filteredProducts
+								}));
+							}}
+						>
+							S
+						</button>
+						<button
+							className={this.state.filters.size === 'M' ? 'active' : ''}
+							onClick={() => {
+								let filteredProducts = this.filterBySize(
+									this.state.filteredProducts,
+									'M'
+								);
+								this.setState(() => ({
+									filters: { ...this.state.filters, size: 'M' },
+									filteredProducts
+								}));
+							}}
+						>
+							M
+						</button>
+						<button
+							className={this.state.filters.size === 'L' ? 'active' : ''}
+							onClick={() => {
+								let filteredProducts = this.filterBySize(
+									this.state.filteredProducts,
+									'L'
+								);
+								this.setState(() => ({
+									filters: { ...this.state.filters, size: 'L' },
+									filteredProducts
+								}));
+							}}
+						>
+							L
+						</button>
+						<button
+							className={this.state.filters.size === 'XL' ? 'active' : ''}
+							onClick={() => {
+								let filteredProducts = this.filterBySize(
+									this.state.filteredProducts,
+									'XL'
+								);
+								this.setState(() => ({
+									filters: { ...this.state.filters, size: 'XL' },
+									filteredProducts
+								}));
+							}}
+						>
+							XL
+						</button>
+						<button
+							className={this.state.filters.size === 'XXL' ? 'active' : ''}
+							onClick={() => {
+								let filteredProducts = this.filterBySize(
+									this.state.filteredProducts,
+									'XXL'
+								);
+								this.setState(() => ({
+									filters: { ...this.state.filters, size: 'XXL' },
+									filteredProducts
+								}));
+							}}
+						>
+							XXL
+						</button>
+					</div>
+					{/*
           <div id="colorFilter" className="color">
             <label htmlFor="color">Color: </label>
             <input
@@ -93,143 +144,175 @@ class App extends React.Component {
               value={this.state.filters.color}
               onChange={this.updateColor}
             />
-          </div>
-          <div id="genderFilter" className="gender-selector">
-            <button
-              onClick={() => this.setState({ filters: { ...this.state.filters, gender: 'male' } })}
-              className={this.state.filters.gender === 'male' ? 'active' : ''}
-            >
-              Male
-            </button>
-            <button
-              onClick={() =>
-                this.setState({ filters: { ...this.state.filters, gender: 'female' } })
-              }
-              className={this.state.filters.gender === 'female' ? 'active' : ''}
-            >
-              Female
-            </button>
-          </div>
-          <div id="layoutOptions" className="layout-selector">
-            <button
-              onClick={() => this.setState({ columns: 3 })}
-              className={this.state.columns === 3 ? 'active' : ''}
-            >
-              3 Columns
-            </button>
-            <button
-              onClick={() => this.setState({ columns: 5 })}
-              className={this.state.columns === 5 ? 'active' : ''}
-            >
-              5 Columns
-            </button>
-          </div>
-        </div>
+          </div>*/}
+					<div id="genderFilter" className="gender-selector">
+						<button
+							onClick={() => this.updateGender('male')}
+							className={this.state.filters.gender === 'male' ? 'active' : ''}
+						>
+							Male
+						</button>
+						<button
+							onClick={() => this.updateGender('female')}
+							className={this.state.filters.gender === 'female' ? 'active' : ''}
+						>
+							Female
+						</button>
+					</div>
+					<div id="layoutOptions" className="layout-selector">
+						<button
+							onClick={() => this.setState({ columns: 3 })}
+							className={this.state.columns === 3 ? 'active' : ''}
+						>
+							3 Columns
+						</button>
+						<button
+							onClick={() => this.setState({ columns: 5 })}
+							className={this.state.columns === 5 ? 'active' : ''}
+						>
+							5 Columns
+						</button>
+					</div>
+				</div>
 
-        <div className="row no-negative-margin">
-          {this.state.products.map((product) => (
-            <Product key={product.id} {...product} columns={this.state.columns} />
-          ))}
-        </div>
-      </OverlayLoader>
-    );
-  }
+				<div className="row no-negative-margin">
+					{this.state.filteredProducts.map(product => (
+						<Product
+							key={product.id}
+							{...product}
+							columns={this.state.columns}
+						/>
+					))}
+				</div>
+			</OverlayLoader>
+		);
+	}
 
-  componentDidMount() {
-    this.fetchData(this.state.count).then((products) =>
-      this.setState(() => ({ products, loading: false }))
-    );
+	componentDidMount() {
+		this.fetchData(this.state.count).then(products =>
+			this.setState(() => ({
+				products,
+				filteredProducts: products,
+				loading: false
+			}))
+		);
 
-    window.addEventListener('scroll', this.handleScroll);
-    this.documentObj = document.documentElement;
-  }
+		window.addEventListener('scroll', this.handleScroll);
+		this.documentObj = document.documentElement;
+	}
 
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
+	componentWillUnmount() {
+		window.removeEventListener('scroll', this.handleScroll);
+	}
 
-  fetchData(page) {
-    const fetchUrl = `${DATA_URL}&pageNumber=${page}`;
+	fetchData(page) {
+		const fetchUrl = `${DATA_URL}&pageNumber=${page}`;
 
-    const headers = {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    };
+		const headers = {
+			Accept: 'application/json',
+			'Content-Type': 'application/json'
+		};
 
-    const body = JSON.stringify({
-      newFilters: [],
-      sort_by: ['relevance']
-    });
+		const body = JSON.stringify({
+			newFilters: [],
+			sort_by: ['relevance']
+		});
 
-    return fetch(fetchUrl, {
-      method: 'POST',
-      headers,
-      body
-    })
-      .then((response) => response.json())
-      .then((response) =>
-        response
-          .filter((item) => item.id !== -2) // remove the search metadata object from the list of products
-          .map((item) => item.ecommerceProductJAXB)
-          .map(
-            // get only the properties you need
-            ({
-              id,
-              name,
-              description,
-              image_pid,
-              salesPrice,
-              retailPrice,
-              discount,
-              color,
-              size,
-              availableSizes,
-              stock,
-              available,
-              category,
-              subCategory,
-              gender,
-              webLink
-            }) => ({
-              id,
-              name,
-              description,
-              image_pid: image_pid.map((imageUrl) => BASE_IMAGE_URL.concat(imageUrl)),
-              salesPrice,
-              retailPrice,
-              discount,
-              color,
-              size,
-              availableSizes,
-              stock,
-              available,
-              category,
-              subCategory,
-              gender,
-              webLink: BASE_WEBLINK_URL.concat(webLink)
-            })
-          )
-      );
-  }
+		return fetch(fetchUrl, {
+			method: 'POST',
+			headers,
+			body
+		})
+			.then(response => response.json())
+			.then(response =>
+				response
+					.filter(item => item.id !== -2) // remove the search metadata object from the list of products
+					.map(item => item.ecommerceProductJAXB)
+					.map(
+						// get only the properties you need
+						({
+							id,
+							name,
+							description,
+							image_pid,
+							salesPrice,
+							retailPrice,
+							discount,
+							color,
+							size,
+							availableSizes,
+							stock,
+							available,
+							category,
+							subCategory,
+							gender,
+							webLink
+						}) => ({
+							id,
+							name,
+							description,
+							image_pid: image_pid.map(imageUrl =>
+								BASE_IMAGE_URL.concat(imageUrl)
+							),
+							salesPrice,
+							retailPrice,
+							discount,
+							color,
+							size,
+							availableSizes,
+							stock,
+							available,
+							category,
+							subCategory,
+							gender,
+							webLink: BASE_WEBLINK_URL.concat(webLink)
+						})
+					)
+			);
+	}
 
-  updateProducts() {
-    this.setState({ loading: true });
+	updateProducts() {
+		this.setState({ loading: true });
 
-    let count = this.state.count + 1;
-    this.fetchData(count).then((newProducts) => {
-      let products = [...this.state.products, ...newProducts];
-      this.setState(() => ({ products, count, loading: false }));
-    });
-  }
+		let count = this.state.count + 1;
+		this.fetchData(count).then(newProducts => {
+			let products = [...this.state.products, ...newProducts];
+			let filteredProducts = this.filterProducts(products, this.state.filters);
+			this.setState(() => ({
+				products,
+				filteredProducts,
+				count,
+				loading: false
+			}));
+		});
+	}
 
-  handleScroll() {
-    var offset = this.documentObj.scrollTop + window.innerHeight;
-    var height = this.documentObj.offsetHeight;
+	handleScroll() {
+		var offset = this.documentObj.scrollTop + window.innerHeight;
+		var height = this.documentObj.offsetHeight;
 
-    if (offset >= height) {
-      this.updateProducts();
-    }
-  }
+		if (offset >= height) {
+			this.updateProducts();
+		}
+	}
+
+	filterProducts(products, filter) {
+		console.log('filterproducts', products, filter);
+		let filteredProducts = this.filterBySize(products, filter.size);
+		filteredProducts = this.filterByGender(filteredProducts, filter.gender);
+		return filteredProducts;
+	}
+
+	filterBySize(products, size) {
+		console.log('filterbysize', products, size);
+		return products.filter(product => product.availableSizes.includes(size));
+	}
+
+	filterByGender(products, gender) {
+		console.log('filterbygender', products, gender);
+
+		return products.filter(product => product.gender === gender);
+	}
 }
 
 export default App;
