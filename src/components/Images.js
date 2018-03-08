4787618c38f3a1467874acc36bf2d3b805a77e30
Manjunatha D <manjunathad13@gmail.com>
2018-03-08 07:45:52 +0530

    Lazy load images

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Lazyload from 'react-lazyload';
 
 class Images extends React.Component {
   constructor(props) {
@@ -16,32 +17,37 @@ class Images extends React.Component {
     const { name } = this.props;
 
     return (
-      <img
-        src={this.state.currentImgSrc}
-        alt={name}
-        onMouseOver={this.startImageTransitions}
-        onMouseOut={this.stopImageTransitions}
-      />
+      <Lazyload once height={200}>
+        <img
+          src={this.state.currentImgSrc}
+          alt={name}
+          onMouseOver={this.startImageTransitions}
+          onMouseOut={this.stopImageTransitions}
+        />
+      </Lazyload>
     );
   }
 
   startImageTransitions() {
-    console.log('mouse enter');
-    let i = 0;
-    this.intervalId = setInterval(() => {
-      this.changeImageSource(i++);
-    }, 1500);
+    let i = 1;
+
+    if (this.props.images.length > 1) {
+      this.intervalId = setInterval(() => {
+        this.changeImageSource(i++);
+      }, 1000);
+    }
   }
 
   changeImageSource(i) {
     let index = i % this.props.images.length;
-    console.log('new source', i, index, this.props.images[index]);
+    // console.log('new source', i, index, this.props.images[index]);
     this.setState(() => ({ currentImgSrc: this.props.images[index] }));
   }
 
   stopImageTransitions() {
-    console.log('mouseleave');
+    // console.log('mouseleave');
     clearInterval(this.intervalId);
+    // this.setState(() => ({ currentImgSrc: this.props.images[0] }));
   }
 }
 
